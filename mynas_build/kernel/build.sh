c1518501f9fa8ecd741bfc2b423cfbebe0f0860a
--==RIX==-- <zeze0556@gmail.com>
2025-12-06 02:08:51 +0000

    内核:6.18 bcachefs版本： 1.33.0

--- mynas_build/kernel/build.sh
+++ mynas_build/kernel/build.sh
@@ -6,7 +6,7 @@ bcachefs_dir="./bcachefs"
 bcachefs_tools="./bcachefs-tools"
 
 KERNEL_VERSION="v6.16"
-TOOLS_VERSION="v1.31.12"
+TOOLS_VERSION="v1.33.0"
 LOCALVERSION="-rix"
 #LOCALVERSION="-generic"
 #KERNEL_SOURCE="https://evilpiepirate.org/git/bcachefs.git"
